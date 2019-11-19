9840fd359d14444ab241ecf773eff4f83a3b1028
Toby Slight <tslight@pm.me>
2019-11-19 21:11:01 +0000

    simplify

--- configuration.nix
+++ configuration.nix
@@ -14,19 +14,12 @@
   networking = {
     hostName = "throg"; # Define your hostname.
     # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-    networkmanager.enable = true;
-
-    # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-    # Per-interface useDHCP will be mandatory in the future, so this generated config
-    # replicates the default behaviour.
-    # useDHCP = false;
+    networkmanager.enable = true; # Mutually exclusive with wpa_supplicant.
     interfaces.enp0s25.useDHCP = true;
     interfaces.wls3.useDHCP = true;
-
     # Configure network proxy if necessary
     # proxy.default = "http://user:password@proxy:port/";
     # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
     # Open ports in the firewall.
     # firewall.allowedTCPPorts = [ ... ];
     # firewall.allowedUDPPorts = [ ... ];
