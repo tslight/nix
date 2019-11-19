ad7f89654049d173520c600bea5727e82153035c
tslight <tslight@pm.com>
2019-11-19 07:44:45 +0000

    add pips to main packages script

--- packages.nix
+++ packages.nix
@@ -49,10 +49,27 @@
     nmap
     jdk12
     powertop
-    python3
-    python37Packages.conda
-    python37Packages.pip
-    python37Packages.ipython
+    (python3.withPackages(ps: with ps; [
+      conda
+      pip
+      ipython
+      autopep8
+      black
+      flake8
+      flask
+      importmagic
+      jedi
+      jupyter
+      requests
+      rope
+      setuptools
+      twine
+      virtualenv
+      wheel
+      yapf
+      pylint
+      xonsh
+    ]))
     qbittorrent
     ranger
     ratpoison
