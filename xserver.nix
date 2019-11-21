beb1d5eeb6f733717ae7b75366f1c73f6aeda11f
tslight <tslight@pm.com>
2019-11-21 11:30:44 +0000

    switch to sddm

--- xserver.nix
+++ xserver.nix
@@ -7,26 +7,56 @@
     # Enable touchpad support.
     libinput.enable = true;
     libinput.tapping = false; # disable tap to click
+    libinput.clickMethod = "clickfinger";
+    libinput.middleEmulation = true;
 
     displayManager = {
-      # sddm.enable = true;
-      # slim.enable = true;
-      # Auto login - only works with LightDM
-      auto.enable = true; 
-      auto.user = "toby";
+      # gdm = {
+      #   enable = true;
+      #   autoLogin.enable = true;
+      #   autoLogin.user = "toby";
+      # };
+      # lightdm = {
+      #   enable = true;
+      #   autoLogin.enable = true;
+      #   autoLogin.user = "toby";
+      # };
+      sddm = {
+        enable = true;
+        autoLogin.enable = true;
+        autoLogin.user = "toby";
+      };
+      # slim = {
+      #   enable = true;
+      #   autoLogin.enable = true;
+      #   autoLogin.user = "toby";
+      # };
     };
 
     desktopManager = {
+      # enlightenment.enable = true;
       # gnome3.enable = true;
+      # lumina.enable = true;
+      lxqt.enable = true;
       # mate.enable = true;
       plasma5.enable = true;
-      xfce.enable = true;
+      # xfce.enable = true;
       default = "plasma5";
     };
 
     windowManager = {
+      awesome.enable = true;
       exwm.enable = true;
+      # exwm.enableDefaultConfig = true; # vanilla
+      fvwm.enable = true;
+      # fluxbox.enable = true;
+      i3.enable = true;
+      # icewm.enable = true;
+      jwm.enable = true;
+      openbox.enable = true;
+      # sawfish.enable = true;
       stumpwm.enable = true;
+      windowmaker.enable = true;
       xmonad.enable = true;
       default = "none";
     };
