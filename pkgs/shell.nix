81887915d27e8c3348605ae546429306ffa4534e
tslight <tslight@pm.com>
2020-02-02 13:37:14 +0000

    another way to install

--- pkgs/shell.nix
+++ pkgs/shell.nix
@@ -1,27 +1,33 @@
 { pkgs ? import <nixpkgs> {} }:
-# default.nix
 let
-  # pkgs = import (import <nixpkgs>) {};
-
-  # bashrc = …;
-  # git = …;
-  # tmux = …;
-  # vim = …;
-
   myPkgs = [
-    # Customized packages
-    # bashrc
-    # git
-    # tmux
-    # vim
-
-    # Sourced directly from Nixpkgs
-    pkgs.curl
-    pkgs.htop
+    pkgs.alttab
+    pkgs.ansible
+    pkgs.azure-cli
+    pkgs.bat
+    pkgs.brave # https://github.com/brave/brave-browser/issues/1986
+    pkgs.broot
+    pkgs.chromium
+    pkgs.docker
+    pkgs.docker-compose
+    pkgs.emacs
+    pkgs.exa
+    pkgs.fast-cli
+    pkgs.firefox
+    pkgs.glibcLocales
+    pkgs.go
+    pkgs.home-manager
+    pkgs.mitscheme # choose between this and chez
     pkgs.nix
-    pkgs.pass
-    pkgs.tree
-    pkgs.xclip
+    pkgs.nix-bash-completions
+    pkgs.nix-prefetch-scripts
+    pkgs.nix-zsh-completions
+    pkgs.nixops
+    pkgs.nixpkgs-lint
+    pkgs.powershell
+    pkgs.ranger
+    pkgs.terraform
+    pkgs.tmux
+    pkgs.youtube-dl
   ];
-
 in myPkgs
