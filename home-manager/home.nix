55fa34a1816116a4a3082b9549b1caebda1d95c7
Toby Slight <tslight@pm.me>
2023-10-10 12:18:17 +0100

    whitespace

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -469,7 +469,6 @@ set colored-completion-prefix on
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
 
-
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.05";
 }
