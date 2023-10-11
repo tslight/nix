c067b47eaab00fa9594b0da6f41d28cca60ffc01
Toby Slight <tslight@pm.me>
2023-10-11 18:15:58 +0100

    got mssql to install

--- darwin/hexley/configuration.nix
+++ darwin/hexley/configuration.nix
@@ -2,7 +2,18 @@
   imports = [../configuration.nix];
   homebrew = {
     enable = true;
-    brews = ["azure-cli" "libomp" "msodbcsql" "mssql-tools" ];
+    brews = [
+      "azure-cli"
+      "libomp"
+      "msodbcsql"
+      "mssql-tools"
+    ];
+    extraConfig = ''
+      module Utils
+        ENV['HOMEBREW_ACCEPT_EULA']='y'
+      end
+      brew "mssql-tools"
+    '';
     casks = [ "miniconda" ];
     taps = [ "microsoft/mssql-release" {
       name = "microsoft/mssql-release";
