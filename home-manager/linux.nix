c48a2b3eb03310cdbd0dbcee27a6a8901e936d90
Toby Slight <tslight@pm.me>
2023-10-11 21:36:42 +0100

    more dconf shuffling

--- home-manager/linux.nix
+++ home-manager/linux.nix
@@ -41,6 +41,38 @@
   systemd.user.startServices = "sd-switch";
 
   dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      gtk-key-theme = "Emacs";
+      clock-show-weekday = true;
+    };
+    "org/gnome/desktop/wm/keybindings" = {
+      close = ["<Super>q"];
+      toggle-maximized = ["<Super>m"];
+    };
+    "org/gnome/shell" = {
+      disable-user-extensions = false;
+      enable-hot-corners = true;
+      enabled-extensions = [
+        "apps-menu@gnome-shell-extensions.gcampax.github.com"
+        "caffeine@patapon.info"
+        "clipboard-indicator@tudmotu.com"
+        "dash-to-dock@micxgx.gmail.com"
+        "drive-menu@gnome-shell-extensions.gcampax.github.com"
+        "places-menu@gnome-shell-extensions.gcampax.github.com"
+        "screenshot-window-sizer@gnome-shell-extensions.gcampax.github.com"
+        "user-theme@gnome-shell-extensions.gcampax.github.com"
+        "windowsNavigator@gnome-shell-extensions.gcampax.github.com"
+        "workspace-indicator@gnome-shell-extensions.gcampax.github.com"
+      ];
+      favorite-apps = [
+        "org.gnome.Nautilus.desktop"
+        "chromium.desktop"
+        "firefox.desktop"
+        "emacs.desktop"
+        "kitty.desktop"
+      ];
+    };
     "org/gnome/settings-daemon/plugins/media-keys" = {
       custom-keybindings = [
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
@@ -63,34 +95,17 @@
       command = "firefox";
       name = "open-firefox";
     };
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-      gtk-key-theme = "Emacs";
-    };
-    "org/gnome/shell" = {
-      disable-user-extensions = false;
-      enabled-extensions = [
-        "apps-menu@gnome-shell-extensions.gcampax.github.com"
-        "caffeine@patapon.info"
-        "clipboard-indicator@tudmotu.com"
-        "dash-to-dock@micxgx.gmail.com"
-        "drive-menu@gnome-shell-extensions.gcampax.github.com"
-        "places-menu@gnome-shell-extensions.gcampax.github.com"
-        "screenshot-window-sizer@gnome-shell-extensions.gcampax.github.com"
-        "user-theme@gnome-shell-extensions.gcampax.github.com"
-        "windowsNavigator@gnome-shell-extensions.gcampax.github.com"
-        "workspace-indicator@gnome-shell-extensions.gcampax.github.com"
-      ];
-      favorite-apps = [
-        "org.gnome.Nautilus.desktop"
-        "chromium.desktop"
-        "firefox.desktop"
-        "emacs.desktop"
-        "kitty.desktop"
-      ];
+    "org/gnome/shell/extensions/dash-to-dock" = {
+      custom-theme-shrink = true;
+      dash-max-icon-size = 32;
+      dock-fixed = true;
+      dock-position = "LEFT";
+      extend-height = true;
+      icon-size-fixed = true;
+      multi-monitor = true;
+      show-trash=false;
     };
   };
-
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.05";
