2e9ed182827ed39196fb5476273412f2e55fb346
tslight <tslight@pm.com>
2025-12-05 20:57:54 +0000

    add firefox/librewolf configuration

--- configuration.nix
+++ configuration.nix
@@ -101,6 +101,86 @@
   nixpkgs.config.allowUnfree = true;
   programs.zsh.enable = true;
   programs.niri.enable = true;
+  programs.firefox = { # https://discourse.nixos.org/t/declare-firefox-extensions-and-settings/36265
+    enable = true;
+    package = pkgs.librewolf;
+    policies = { # https://mozilla.github.io/policy-templates/
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+        EmailTracking = true;
+        SuspectedFingerprinting = true;
+        Category = "strict";
+      };
+      HttpsOnlyMode = "enabled";
+      Preferences = { # about:config
+        "browser.ctrlTab.sortByRecentlyUsed" = true;
+        "browser.engagement.ctrlTab.has-used" = true;
+        "browser.engagement.sidebar-button.has-used" = true;
+        "browser.safebrowsing.downloads.remote.block_dangerous" = true;
+        "browser.safebrowsing.downloads.remote.block_dangerous_host" = true;
+        "browser.safebrowsing.malware.enabled" = true;
+        "browser.safebrowsing.phishing.enabled" = true;
+        "browser.toolbarbuttons.introduced.sidebar-button" = true;
+        "browser.theme.dark-private-windows" = true;
+        "cookiebanners.service.mode" = 2; # Block cookie banners
+        "cookiebanners.service.mode.privateBrowsing" = 2; # Block cookie banners in private browsing
+        "dom.security.https_only_mode_ever_enabled" = true;
+        "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+        "privacy.annotate_channels.strict_list.enabled" = true;
+        "privacy.bounceTrackingProtection.hasMigratedUserActivationData" = true;
+        "privacy.bounceTrackingProtection.mode" = 1;
+        "privacy.clearOnShutdown.cache" = false;
+        "privacy.clearOnShutdown.cookies" = false;
+        "privacy.clearOnShutdown.downloads" = false;
+        "privacy.clearOnShutdown.history" = false;
+        "privacy.clearOnShutdown.sessions" = false;
+        "privacy.clearOnShutdown.siteSettings" = false;
+        "privacy.clearOnShutdown_v2.browsingHistoryAndDownloads" = false;
+        "privacy.clearOnShutdown_v2.cache" = false;
+        "privacy.clearOnShutdown_v2.cookiesAndStorage" = false;
+        "privacy.donottrackheader.enabled" = true;
+        "privacy.fingerprintingProtection" = true;
+        "privacy.globalprivacycontrol.was_ever_enabled" = true;
+        "privacy.resistFingerprinting" = true;
+        "privacy.trackingprotection.allow_list.convenience.enabled" = false;
+        "privacy.trackingprotection.emailtracking.enabled" = true;
+        "privacy.trackingprotection.enabled" = true;
+        "privacy.trackingprotection.fingerprinting.enabled" = true;
+        "privacy.trackingprotection.socialtracking.enabled" = true;
+        "sidebar.new-sidebar.has-used" = true;
+        "sidebar.revamp" = true;
+        "sidebar.verticalTabs" = true;
+        "sidebar.verticalTabs.dragToPinPromo.dismissed" = true;
+        "webgl.disabled" = false;
+      };
+      ExtensionSettings = { # about:support
+        "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
+        # uBlock Origin:
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        # Privacy Badger:
+        "jid1-MnnxcxisBPnSXQ@jetpack" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium/latest.xpi";
+          installation_mode = "force_installed";
+        };
+      };
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     aspell
     aspellDicts.en
@@ -119,7 +199,6 @@
     inxi
     jq
     kitty
-    librewolf
     ncdu
     neovim
     playerctl
