5bd2cae175a046507bbad560b7fd4583fe0082bf
Toby Slight <tslight@pm.me>
2025-12-13 19:27:19 +0000

    fix install

--- flake.nix
+++ flake.nix
@@ -3,13 +3,25 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/25.11";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, home-manager, nixpkgs }: {
     nixosConfigurations = {
       enigma = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
+          home-manager.nixosModules.home-manager {
+            home-manager.useUserPackages = true;
+            home-manager.users.anon = {
+              imports = [
+                ./home/default.nix
+              ];
+            };
+          }
           ./enigma/hardware-configuration.nix
           ./laptop.nix
         ];
