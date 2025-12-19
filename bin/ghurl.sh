10113cad00bc244784ce737adf8e582e4265d8f5
Toby Slight <tslight@pm.me>
2025-12-19 23:27:49 +0000

    delete unnecessary scripts

--- bin/ghurl.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-git remote set-url origin git@github.com:tslight/nix
-git remote -v
--- bin/install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sudo nixos-rebuild switch --flake .
-home-manager switch --flake ./home
