b87a5f1f45274b17442152a622844497249a72bd
Toby Slight <tslight@pm.me>
2025-12-19 21:24:23 +0000

    add cardiel

--- cardiel/hardware-configuration.nix
+++ cardiel/hardware-configuration.nix
@@ -37,5 +37,16 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-  networking.hostname = "cardiel";
+  networking.hostName = "cardiel";
+  services.udev.extraHwdb = ''
+  evdev:input:b0011v0001p0001*
+    KEYBOARD_KEY_01=capslock
+    KEYBOARD_KEY_3a=esc
+    KEYBOARD_KEY_b8=leftctrl
+    KEYBOARD_KEY_38=leftctrl
+    KEYBOARD_KEY_b7=leftalt
+    KEYBOARD_KEY_db=leftalt
+    KEYBOARD_KEY_9d=leftmeta
+    KEYBOARD_KEY_1d=leftmeta
+  '';
 }
