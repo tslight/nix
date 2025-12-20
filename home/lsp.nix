767114c523d87bc1efc71b8971ad8416bfcb7bc1
Toby Slight <tslight@pm.me>
2025-12-20 04:12:21 +0000

    more language servers please baby

--- home/lsp.nix
+++ home/lsp.nix
@@ -2,18 +2,20 @@
 
 {
   home.packages = [
-    pkgs.nodePackages.vscode-langservers-extracted
-    pkgs.nodePackages.javascript-typescript-langserver
-    pkgs.ruff
-    pkgs.ruby-lsp
-    pkgs.gopls
-    pkgs.rust-analyzer
+    pkgs.bash-language-server
     pkgs.ccls
-    pkgs.phpactor
+    pkgs.copilot-language-server
+    pkgs.gopls
     pkgs.lua-language-server
+    pkgs.nixd
+    pkgs.nodePackages.javascript-typescript-langserver
+    pkgs.nodePackages.vscode-langservers-extracted
+    pkgs.phpactor
+    pkgs.ruby-lsp
+    pkgs.ruff
+    pkgs.rust-analyzer
+    pkgs.shfmt
     pkgs.vscode-langservers-extracted
     pkgs.yaml-language-server
-    pkgs.bash-language-server
-    pkgs.shfmt
   ];
 }
