2587936722250c5d1907216be67f464371e1f545
Toby Slight <tslight@pm.me>
2025-12-20 03:34:18 +0000

    fix neovim keybindings

--- home/dotfiles/nvim/init.lua
+++ home/dotfiles/nvim/init.lua
@@ -3,6 +3,9 @@ vim.cmd.colorscheme("wildcharm")
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>w", vim.cmd.write, { desc = "Write" })
 vim.keymap.set("n", "<leader>q", vim.cmd.quit, { desc = "Quit" })
+vim.keymap.set("n", "<leader>bn", ':bn<cr>', { desc = "Buffer Next" })
+vim.keymap.set("n", "<leader>bp", ':bp<cr>', { desc = "Buffer Prev" })
+vim.keymap.set("n", "<leader>bl", ':bl<cr>', { desc = "Buffer Last" })
 vim.opt.cursorline = true
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -20,16 +23,18 @@ vim.pack.add({
 })
 
 require('fzf-lua').setup{}
+require('which-key').setup{}
+
 vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "Files Project" })
 vim.keymap.set("n", "<leader>f.", "<cmd>FzfLua files cwd=.<cr>", { desc = "Files Here" })
 vim.keymap.set("n", "<leader>fh", "<cmd>FzfLua files cwd=~<cr>", { desc = "Files Home" })
-vim.keymap.set("n", "<leader>c", "<cmd>FzfLua commands<cr>", { desc = "Commands" })
+vim.keymap.set("n", "<leader>x", "<cmd>FzfLua commands<cr>", { desc = "Commands" })
+vim.keymap.set("n", "<leader>c", ":CopilotChat<cr>", { desc = "Commands" })
 vim.keymap.set("n", "<leader>r", "<cmd>FzfLua oldfiles<cr>", { desc = "Recent" })
-vim.keymap.set("n", "<leader>b", "<cmd>FzfLua buffers<cr>", { desc = "Buffers" })
+vim.keymap.set("n", "<leader>bb", "<cmd>FzfLua buffers<cr>", { desc = "Buffers" })
 vim.keymap.set("n", "<leader>h", "<cmd>FzfLua helptags<cr>", { desc = "Help" })
 vim.keymap.set("n", "<leader>m", "<cmd>FzfLua manpages<cr>", { desc = "Man" })
 vim.keymap.set("n", "<leader>s", "<cmd>FzfLua grep_project<cr>", { desc = "Grep Project" })
 vim.keymap.set("n", "<leader> ", "<cmd>FzfLua global<cr>", { desc = "FZF" })
-require('which-key').setup{}
-vim.keymap.set('n', '<leader>g', ':LazyGit<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>e', ':Yazi<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>g', ':LazyGit<CR>', { desc = "Git", noremap = true, silent = true })
+vim.keymap.set('n', '<leader>e', ':Yazi<CR>', { desc = "Explorer", noremap = true, silent = true })
